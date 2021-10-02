c50e44b16531936b145c1784106ac0bc4fca1c8c
jimmykiang <castlevanity@gmail.com>
2021-10-02 17:51:42 +0200

    (s *SphSolver3) onBeginAdvanceTimeStep

--- SphSystemData3.go
+++ SphSystemData3.go
@@ -137,3 +137,85 @@ func (s *SphSystemData3) forces() []*Vector3D.Vector3D {
 
 	return (*s).particleSystemData.vectorDataList[s.particleSystemData.forceIdx]
 }
+
+func (s *SphSystemData3) densities() []float64 {
+	return (*s).particleSystemData.scalarDataList[s.densityIdx]
+}
+
+func (s *SphSystemData3) buildNeighborSearcher() {
+	// Use PointParallelHashGridSearcher2 by default... (now PointParallelHashGridSearcher3).
+	s.particleSystemData.neighborSearcher = NewPointParallelHashGridSearcher3(
+		constants.KDefaultHashGridResolution,
+		constants.KDefaultHashGridResolution,
+		constants.KDefaultHashGridResolution,
+		2*s.kernelRadius,
+	)
+
+	size := int(s.particleSystemData.neighborSearcher.resolution.X * s.particleSystemData.neighborSearcher.resolution.Y)
+
+	for i := 0; i < size-1; i++ {
+		s.particleSystemData.neighborSearcher.startIndexTable = append(
+			s.particleSystemData.neighborSearcher.startIndexTable,
+			int64(math.MaxInt64),
+		)
+
+		s.particleSystemData.neighborSearcher.endIndexTable = append(
+			s.particleSystemData.neighborSearcher.endIndexTable,
+			int64(math.MaxInt64),
+		)
+	}
+
+	s.particleSystemData.neighborSearcher.build(s.positions())
+}
+
+func (s *SphSystemData3) buildNeighborLists() {
+
+	s.particleSystemData.neighborLists = make([][]int64, 0, 0)
+
+	for i := 0; i < int(s.particleSystemData.numberOfParticles); i++ {
+		s.particleSystemData.neighborLists = append(s.particleSystemData.neighborLists, make([]int64, 0, 0))
+	}
+
+	points := s.positions()
+
+	// Callback function for nearby search query. The first parameter is the
+	// index of the nearby point, and the second is the position of the point.
+	callback := func(i, j int64, v *Vector3D.Vector3D, origin *Vector3D.Vector3D, sum *float64) {
+		if i != j {
+			s.particleSystemData.neighborLists[i] = append(s.particleSystemData.neighborLists[i], j)
+		}
+	}
+
+	for i := int64(0); i < s.particleSystemData.numberOfParticles; i++ {
+
+		//println("buildNeighborLists:", i)
+		origin := points[i]
+		s.particleSystemData.neighborLists[i] = make([]int64, 0, 0)
+		s.particleSystemData.neighborSearcher.forEachNearbyPoint(origin, s.kernelRadius, i, nil, callback)
+	}
+}
+
+func (s *SphSystemData3) updateDensities() {
+
+	p := s.positions()
+	d := s.densities()
+	m := s.particleSystemData.Mass()
+
+	for i := int64(0); i < s.particleSystemData.numberOfParticles; i++ {
+		sum := s.sumOfKernelNearby(p[i])
+		d[i] = m * sum
+	}
+}
+
+func (s *SphSystemData3) sumOfKernelNearby(origin *Vector3D.Vector3D) float64 {
+	sum := 0.0
+	kernel := NewSphStdKernel3(s.kernelRadius)
+
+	callback := func(i, j int64, neighborPosition *Vector3D.Vector3D, origin *Vector3D.Vector3D, sum *float64) {
+		dist := origin.DistanceTo(neighborPosition)
+		*sum += kernel.operatorKernel(dist)
+	}
+
+	s.particleSystemData.neighborSearcher.forEachNearbyPoint(origin, s.kernelRadius, 0, &sum, callback)
+	return sum
+}
